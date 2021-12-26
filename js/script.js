fec62af9244a18780570cc9cf0ec7f7395d606d2
xhentu <heinchanthu@yahoo.com>
2021-12-26 21:43:01 +0630

    js

--- js/script.js
+++ js/script.js
@@ -80,9 +80,9 @@ audioBtn.addEventListener('click', () => {
     audioBtn.style.border = "none";
   };
 });
-const doit = () => {
-  audio.src = "assets\music\MoonlightSonata.mp3";
-  audio.play();
-}
+// const doit = () => {
+//   audio.src = "assets\music\MoonlightSonata.mp3";
+//   audio.play();
+// }
 // audio.src = "assets\music\MoonlightSonata.mp3";
 // audio.play();
